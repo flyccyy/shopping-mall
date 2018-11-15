a81ba1b73ed49ccdabaf0677965bd8847ba1ebff
fly <fly@163.com>
2018-11-15 16:06:03 +0800

    modify

---
+++ src/page/login/index.js
@@ -0,0 +1,9 @@
+/*
+* @Author: Fly
+* @Date:   2018-11-14 10:54:01
+* @Last Modified by:   Fly
+* @Last Modified time: 2018-11-14 12:13:44
+*/
+'use strict';
+require('../module.js');
+console.log('hello login');
